59c54f89fb4368f11b1263210a3f1c2a399cc602
ysfj32ni <ysfjaadouni@gmail.com>
2023-03-23 07:22:48 -0700

    hello from C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,14 @@
 
 /**
  * main - print to the standard error .
- * 
+ *
  * Return:  1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			, stderr);
 	return (1);
 }
 
