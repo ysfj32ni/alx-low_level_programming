8a8ad382b1282847df89ab74a43353f62417ffe3
ysfj32ni <ysfjaadouni@gmail.com>
2023-03-30 10:21:33 -0700

    strcap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,27 @@
  */
 char *cap_string(char *str)
 {
-	int	i;
+	int i = 0;
 
-	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			str[i] += 32;
-		}
-		i++;
-	}
-	i = 0;
-	if (str[0] != '\0' && str[0] >= 'a' && str[0] <= 'z')
-		str[0] -= 32;
-	i++;
-	while (str[i - 1] != '\0')
-	{
-		if (str[i - 1] < 'a' || str[i - 1] > 'z')
-			if (str[i - 1] < 'A' || str[i - 1] > 'Z')
-				if (str[i - 1] < '0' || str[i - 1] > '9')
-					if (str[i] >= 'a' && str[i] <= 'z')
-						str[i] -= 32;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+			str[i] -= 32;
 		i++;
 	}
 	return (str);
